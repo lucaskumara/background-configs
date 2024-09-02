afc02a835cc84d08094250a36587841894bfb476
Lucas Kumara <lucaskumara@hotmail.com>
2024-09-02 12:52:00 -0400

    Added capture return value decorator

--- src/retailers/walmart/script.ts
+++ src/retailers/walmart/script.ts
@@ -1,10 +1,11 @@
 import { StartData } from "../../utils/models/startdata";
-import { applyConverters } from "../../utils/decorators";
+import { applyConverters, captureReturnValueAsync } from "../../utils/decorators";
 import { CollectOrdersConverter, StartDataConverter } from "../../utils/converters";
 import { Logger } from "../../utils/logging";
 
 export class Walmart {
 
+    @captureReturnValueAsync
     @applyConverters([new StartDataConverter(), new CollectOrdersConverter()])
     static async collectOrderDetails(startData: StartData, collectOrders: boolean): Promise<Order[]> {
         // To stop eslint from complaining there is no await
--- src/utils/decorators.ts
+++ src/utils/decorators.ts
@@ -7,6 +7,7 @@ eslint-disable
 */
 
 import { Converter } from "./converters";
+import { Logger } from "./logging";
 
 export function applyConverters(converters: Converter<any>[]) {
     return function (unused1: any, unused2: string, descriptor: PropertyDescriptor) {
@@ -20,4 +21,24 @@ export function applyConverters(converters: Converter<any>[]) {
             return originalMethod.apply(this, args);
         };
     }
+}
+
+export function captureReturnValueAsync(unused1: any, unused2: any, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (...args: any[]): Promise<any> {
+        const result = await originalMethod.apply(this, args);
+
+        console.log(Logger.debugMessages);
+        console.log(Logger.infoMessages);
+        console.log(Logger.warningMessages);
+        console.log(Logger.errorMessages);
+
+        // Send back the result
+        // message_callback_body(...)
+
+        return result;
+    };
+
+    return descriptor;
 }
